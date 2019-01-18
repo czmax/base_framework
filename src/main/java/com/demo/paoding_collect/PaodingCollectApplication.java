354ca53480955f03194e3474431a756a1ed1f543
max.chen <amingccc@gmail.com>
2019-01-18 20:18:06 +0800

    first commit

---
+++ src/main/java/com/demo/paoding_collect/PaodingCollectApplication.java
@@ -0,0 +1,14 @@
+package com.demo.paoding_collect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaodingCollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaodingCollectApplication.class, args);
+	}
+
+}
+
